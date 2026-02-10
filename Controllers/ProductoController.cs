c2959a376db2620358da5430b59bd60465762356
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-10 14:00:55 -0500

    commit - ProductoController .

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+using ProductosAlimenticiosMVC.Data;
+using ProductosAlimenticiosMVC.Models;
+
+namespace ProductosAlimenticiosMVC.Controllers
+{
+    public class ProductoController : Controller
+    {
+        ProductoDAO dao = new ProductoDAO();
+
+        public ActionResult Index()
+        {
+            return View(dao.Listar());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Producto p)
+        {
+            dao.EjecutarSP(2, p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(Producto p)
+        {
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult EditConfirm(Producto p)
+        {
+            dao.EjecutarSP(3, p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(Producto p)
+        {
+            dao.EjecutarSP(4, p);
+            return RedirectToAction("Index");
+        }
+    }
+}
