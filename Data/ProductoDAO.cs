792489c20221c451a51e8dfef7f7eece8ccf3a7f
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-10 14:25:04 -0500

    commit - ProductoDAO .

--- Data/ProductoDAO.cs
+++ Data/ProductoDAO.cs
@@ -1,63 +1,80 @@
 ﻿using ProductosAlimenticiosMVC.Models;
+using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using System.Data;
 
 namespace ProductosAlimenticiosMVC.Data
 {
     public class ProductoDAO
     {
-        string cadena =
-        "User Id=system;" +
-        "Password=Tapiero123;" +
-        "Data Source=(DESCRIPTION=" +
-        "(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))" +
-        "(CONNECT_DATA=(SERVICE_NAME=orcl))" +
-        ");";   
+        private string cadena =
+            "User Id=system;" +
+            "Password=Tapiero123;" +
+            "Data Source=localhost:1521/orcl;";
 
+        // ===============================
+        // LISTAR (OPCION = 1)
+        // ===============================
         public List<Producto> Listar()
         {
             List<Producto> lista = new List<Producto>();
 
-            using (SqlConnection cn = new SqlConnection(cadena))
+            using (OracleConnection cn = new OracleConnection(cadena))
             {
-                SqlCommand cmd = new SqlCommand("SP_PRODUCTO_CRUD", cn);
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@OPCION", 1);
+                OracleCommand cmd = new OracleCommand("SP_PRODUCTO_CRUD", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("P_OPCION", OracleDbType.Int32).Value = 1;
+
+                cmd.Parameters.Add("P_CURSOR", OracleDbType.RefCursor)
+                               .Direction = ParameterDirection.Output;
 
                 cn.Open();
-                SqlDataReader dr = cmd.ExecuteReader();
 
-                while (dr.Read())
+                using (OracleDataReader dr = cmd.ExecuteReader())
                 {
-                    lista.Add(new Producto
+                    while (dr.Read())
                     {
-                        IdProducto = (int)dr["ID_PRODUCTO"],
-                        Nombre = dr["NOMBRE"].ToString(),
-                        Categoria = dr["CATEGORIA"].ToString(),
-                        Precio = (decimal)dr["PRECIO"],
-                        Stock = (int)dr["STOCK"],
-                        FechaVencimiento = (DateTime)dr["FECHA_VENCIMIENTO"]
-                    });
+                        lista.Add(new Producto
+                        {
+                            IdProducto = Convert.ToInt32(dr["ID_PRODUCTO"]),
+                            Nombre = dr["NOMBRE"].ToString(),
+                            Categoria = dr["CATEGORIA"].ToString(),
+                            Precio = Convert.ToDecimal(dr["PRECIO"]),
+                            Stock = Convert.ToInt32(dr["STOCK"]),
+                            FechaVencimiento = Convert.ToDateTime(dr["FECHA_VENCIMIENTO"])
+                        });
+                    }
                 }
             }
+
             return lista;
         }
 
+        // ===============================
+        // INSERTAR / ACTUALIZAR / ELIMINAR
+        // ===============================
         public void EjecutarSP(int opcion, Producto p)
         {
-            using (SqlConnection cn = new SqlConnection(cadena))
+            using (OracleConnection cn = new OracleConnection(cadena))
             {
-                SqlCommand cmd = new SqlCommand("SP_PRODUCTO_CRUD", cn);
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                OracleCommand cmd = new OracleCommand("SP_PRODUCTO_CRUD", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.AddWithValue("@OPCION", opcion);
-                cmd.Parameters.AddWithValue("@ID_PRODUCTO", p.IdProducto);
-                cmd.Parameters.AddWithValue("@NOMBRE", p.Nombre);
-                cmd.Parameters.AddWithValue("@CATEGORIA", p.Categoria);
-                cmd.Parameters.AddWithValue("@PRECIO", p.Precio);
-                cmd.Parameters.AddWithValue("@STOCK", p.Stock);
-                cmd.Parameters.AddWithValue("@FECHA_VENCIMIENTO", p.FechaVencimiento);
+                cmd.Parameters.Add("P_OPCION", OracleDbType.Int32).Value = opcion;
+                cmd.Parameters.Add("P_ID_PRODUCTO", OracleDbType.Int32).Value =
+                    p.IdProducto == 0 ? (object)DBNull.Value : p.IdProducto;
+
+                cmd.Parameters.Add("P_NOMBRE", OracleDbType.Varchar2).Value = p.Nombre;
+                cmd.Parameters.Add("P_CATEGORIA", OracleDbType.Varchar2).Value = p.Categoria;
+                cmd.Parameters.Add("P_PRECIO", OracleDbType.Decimal).Value = p.Precio;
+                cmd.Parameters.Add("P_STOCK", OracleDbType.Int32).Value = p.Stock;
+                cmd.Parameters.Add("P_FECHA_VENCIMIENTO", OracleDbType.Date).Value = p.FechaVencimiento;
+
+                // Cursor obligatorio aunque no se use
+                cmd.Parameters.Add("P_CURSOR", OracleDbType.RefCursor)
+                               .Direction = ParameterDirection.Output;
 
                 cn.Open();
                 cmd.ExecuteNonQuery();
